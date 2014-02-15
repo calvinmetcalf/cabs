1f89f4fc26e12539203427138239208c3e477850
Max Ogden <max@maxogden.com>
2014-02-15 15:25:50 -0500

    add a simple benchmark

---
+++ bench/shasum.js
@@ -0,0 +1,20 @@
+var crypto = require('crypto')
+var fs = require('fs')
+var path = require('path')
+
+var hasher =  crypto.createHash('sha1')
+var file = fs.createReadStream(path.join(process.cwd(), process.argv[2]))
+
+console.time('hash')
+
+file.on('close', function() {
+  console.log(hasher.read().toString('hex'))
+  console.timeEnd('hash')
+})
+
+file.pipe(hasher)
+
+function hashes() {
+  hash.write(input);
+  hash.end();
+}
